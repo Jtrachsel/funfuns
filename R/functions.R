284cf6ae63654bf87fe224d669df97690f2af87b
jtrachsel <julestrachsel@gmail.com>
2018-03-10 14:59:59 -0600

    add error messages to NMDS_ellipse function.

--- R/functions.R
+++ R/functions.R
@@ -253,55 +253,61 @@ pairwise.adonis <- function(x,factors, sim.method, p.adjust.m){
 #' @examples coming soon
 NMDS_ellipse <- function(metadata, OTU_table, grouping_set, distance_method = 'bray', rand_seed = 77777, MDS_trymax = 1000){
   require(vegan)
-  generic_dist <- vegdist(OTU_table, method = distance_method)
-  set.seed(rand_seed)
-  generic_MDS <- metaMDS(generic_dist, k = 2, trymax = MDS_trymax, autotransform = FALSE)
-  stress <- generic_MDS$stress
-  nmds_points <- as.data.frame(generic_MDS$points)
+  if (grouping_set %in% colnames(metadata)){
+    if (all(rownames(metadata) == rownames(OTU_table))){
 
-  metadata <- metadata[match(rownames(generic_MDS$points), rownames(metadata)),]
+      generic_dist <- vegdist(OTU_table, method = distance_method)
+      set.seed(rand_seed)
+      generic_MDS <- metaMDS(generic_dist, k = 2, trymax = MDS_trymax, autotransform = FALSE)
+      stress <- generic_MDS$stress
+      nmds_points <- as.data.frame(generic_MDS$points)
+      metadata <- metadata[match(rownames(generic_MDS$points), rownames(metadata)),]
+      metanmds <- cbind(metadata, nmds_points)
+      nmds.mean <- aggregate(metanmds[,grep("MDS", colnames(metanmds))], list(group=metanmds[[grouping_set]]), mean)
+      metanmds[[grouping_set]] <- factor(metanmds[[grouping_set]]) # this 'set' needs to be passed in from function
 
-  metanmds <- cbind(metadata, nmds_points)
+      #check to make sure at least 3 obs for each grouping_set
+
+      numobs <- metanmds %>% group_by_(grouping_set) %>% summarise(n=n())
+      if (min(numobs$n) >= 3){
+        ord <- ordiellipse(generic_MDS, metanmds[[grouping_set]], label = TRUE, conf = .95, kind = 'se', draw = 'none')
+
+        df_ell <- data.frame()
+        for (d in levels(metanmds[[grouping_set]])){
+          df_ell <- rbind(df_ell, cbind(as.data.frame(with(metanmds[metanmds[[grouping_set]] == d,],
+                                                           veganCovEllipse(ord[[d]]$cov, ord[[d]]$center, ord[[d]]$scale))),group=d))
+        }
 
 
+        levels(metanmds[[grouping_set]])
 
-  nmds.mean <- aggregate(metanmds[,grep("MDS", colnames(metanmds))], list(group=metanmds[[grouping_set]]), mean)
+        # this loop assigns the group centroid X coordinates to each sample
+        metanmds$centroidX <- NA
+        metanmds$centroidY <- NA
 
-  metanmds[[grouping_set]] <- factor(metanmds[[grouping_set]]) # this 'set' needs to be passed in from function
 
-  #check to make sure at least 3 obs for each grouping_set
+        for (level in levels(metanmds[[grouping_set]])){
+          metanmds[metanmds[[grouping_set]] == level,]$centroidX <- nmds.mean$MDS1[nmds.mean$group == level]
+          metanmds[metanmds[[grouping_set]] == level,]$centroidY <- nmds.mean$MDS2[nmds.mean$group == level]
 
-  numobs <- metanmds %>% group_by_(grouping_set) %>% summarise(n=n())
-  if (min(numobs$n) >= 3){
-    ord <- ordiellipse(generic_MDS, metanmds[[grouping_set]], label = TRUE, conf = .95, kind = 'se', draw = 'none')
 
-    df_ell <- data.frame()
-    for (d in levels(metanmds[[grouping_set]])){
-      df_ell <- rbind(df_ell, cbind(as.data.frame(with(metanmds[metanmds[[grouping_set]] == d,],
-                                                       veganCovEllipse(ord[[d]]$cov, ord[[d]]$center, ord[[d]]$scale))),group=d))
+        }
+        print(paste('Ordination stress:', stress, sep = ' '))
+        return(list(metanmds, df_ell, generic_MDS))
+
+      } else {
+        warning('One of your groups in "grouping_set" has less than 3 observations, cannot generate elipses')
+        df_ell <- data.frame()
+        return(list(metanmds, df_ell, generic_MDS))}
+
+    } else {
+      stop('The rownames for your OTU table and metadata do not match.')
     }
 
+  }else {
+    stop('The grouping set column you have provided in not in your metadata.')
+  }
 
-    levels(metanmds[[grouping_set]])
-
-    # this loop assigns the group centroid X coordinates to each sample
-    metanmds$centroidX <- NA
-    metanmds$centroidY <- NA
-
-
-    for (level in levels(metanmds[[grouping_set]])){
-      metanmds[metanmds[[grouping_set]] == level,]$centroidX <- nmds.mean$MDS1[nmds.mean$group == level]
-      metanmds[metanmds[[grouping_set]] == level,]$centroidY <- nmds.mean$MDS2[nmds.mean$group == level]
-
-
-    }
-    print(paste('Ordination stress:', stress, sep = ' '))
-    return(list(metanmds, df_ell, generic_MDS))
-
-  } else {
-    warning('One of your groups in "grouping_set" has less than 3 observations, cannot generate elipses')
-    df_ell <- data.frame()
-    return(list(metanmds, df_ell, generic_MDS))}
 
 
 }
