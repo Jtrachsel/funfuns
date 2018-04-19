e53a4778a8ceaf84654ea5748baea1890aeec67e
jtrachsel <julestrachsel@gmail.com>
2018-04-19 15:14:46 -0500

    updated pairwise.adonis

--- R/functions.R
+++ R/functions.R
@@ -178,8 +178,6 @@ gather_nodes <- function(x, typ=NA){
 
 }
 
-#' Pairwise adonis
-#'
 #' this function taken from https://www.researchgate.net/post/How_can_I_do_PerMANOVA_pairwise_contrasts_in_R
 #'
 #' @param x An OTU table with taxa as columns and samples as rows
